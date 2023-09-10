fc94db905227ce99664060b13e43c19256cc684e
Fatima Kraiczyi <fatima.kraiczyi@brf.com>
2023-09-10 19:03:12 -0300

    fix: routes

--- DS151/App.js
+++ DS151/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Home from './Home/Home';
-import Counter from './CounterComponent/Counter'
-import ColorList from './ColorListComponent/ColorList';
-import MediaCalculator from './MediaCalculatorComponent/MediaCalculator'
+import Home from './Components/Home/Home';
+import Counter from './Components/CounterComponent/Counter'
+import ColorList from './Components/ColorListComponent/ColorList';
+import MediaCalculator from './Components/MediaCalculatorComponent/MediaCalculator'
 import Galeria from './GaleriaComponent/Galeria';
 
 const Stack = createStackNavigator();
