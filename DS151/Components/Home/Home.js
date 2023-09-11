e50e4cb8e88dbfeffbf0b03cbd9aee88a1151599
Lucas Mello <lucas.mello@livetouch.com.br>
2023-09-11 03:23:42 -0300

    update flatlist

--- DS151/Components/Home/Home.js
+++ DS151/Components/Home/Home.js
@@ -1,12 +1,15 @@
-import React from "react";
 import { Menu, HamburgerIcon, Box, Pressable } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import CustomSearchBar from "../SearchBar/SearchBar";
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, FlatList, Text } from 'react-native';
 import tmdb from "../../src/api/tmdb";
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+    const [titles, setTitles] = useState([]);
+
     async function searchTitle(query) {
+        const titlesResult = [];
         try {
             const response = await tmdb.get('/search/movie', {
                 params: {
@@ -15,20 +18,24 @@ function Home() {
                 }
             })
             const results = response.data.results;
-            const titlesArray = [];
-    
+
             results.forEach(result => {
-                titlesArray.push(result.title);
+                titlesResult.push(result.title);
             });
-    
-            console.log(titlesArray);
+            setTitles(titlesResult)
         } catch (error) {
             console.error('Erro:', error);
         }
     };
-    
-    
+
+    useEffect(() => {
+        // Este efeito será acionado após a atualização de 'titles'
+        console.log('Estado atualizado:', titles);
+    }, [titles]);
+
     const navigation = useNavigation();
+
+
     return (
         <Box w="100%" p={4}>
             <Box flexDirection="column" alignItems="flex-start">
@@ -62,6 +69,14 @@ function Home() {
                 <CustomSearchBar onSearch={(searchText) => {
                     searchTitle(searchText);
                 }} />
+                <FlatList
+                    data={titles}
+                    renderItem={({ item }) => (
+                        <View style={{ padding: 10 }}>
+                            <Text>{item}</Text>
+                        </View>
+                    )}
+                />
             </View>
         </Box>
     );
